964d05571e2c17d407c5fc10c5a7bf0bd8a8d5a7
virginia-kaurai <virginiakaurai.com>
2026-06-07 22:53:12 +0300

    second commit

---
+++ mysite/api/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import BlogPost
+from .serializers import BlogPostSerializer
+
+
+class BlogPostListCreate(generics.ListCreateAPIView):
+    queryset =BlogPost.objects.all()
+    serializer_class=BlogPostSerializer
+
+class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset =BlogPost.objects.all()
+    serializer_class=BlogPostSerializer
+    lookup_field = 'pk'
